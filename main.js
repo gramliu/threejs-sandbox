7a81791e5c928bad5aef97aeff9d6e772fd29f5b
Gram Liu <gram.liu226@gmail.com>
2022-05-07 02:08:45 -0400

    Added stars and space background

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ scene.add(torus);
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(0, 5, 0);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
@@ -38,6 +38,22 @@ const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load("./space.jpg");
+scene.background = spaceTexture;
 
 
 function animate() {
